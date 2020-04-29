415bd2ec00b1279278bc19301b6ec9f4e7953a9e
Junhao Wang <junhaowww@outlook.com>
2020-04-28 18:18:28 -0700

    add MetaData.java

--- ContentBrowser/src/MetaData.java
+++ ContentBrowser/src/MetaData.java
@@ -1,8 +1,39 @@
-/**
- * @author Junhao Wang
- * @date 04/28/2020
- */
-package PACKAGE_NAME;
+import java.util.List;
 
 public class MetaData {
+
+  private int synopsisWidth;
+  private int synopsisHeight;
+  private int synopsisSpan;
+
+  private List<Item> itemList;
+  private List<String> imageFileNameList;
+
+  public MetaData(int w, int h, int s, List<Item> itemList, List<String> fileNameList) {
+    synopsisWidth = w;
+    synopsisHeight = h;
+    synopsisSpan = s;
+    this.itemList = itemList;
+    this.imageFileNameList = fileNameList;
+  }
+
+  public int getSynopsisWidth() {
+    return synopsisWidth;
+  }
+
+  public int getSynopsisHeight() {
+    return synopsisHeight;
+  }
+
+  public int getSynopsisSpan() {
+    return synopsisSpan;
+  }
+
+  public List<Item> getItemList() {
+    return itemList;
+  }
+
+  public List<String> getImageFileNameList() {
+    return imageFileNameList;
+  }
 }
